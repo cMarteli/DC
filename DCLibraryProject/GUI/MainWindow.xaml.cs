1df23dd5beaa75f494872e77d3c044decd22116f
Victor Marteli <caio.marteli@proton.me>
2023-08-18 09:04:20 +0800

    minor fixes

--- DCLibraryProject/GUI/MainWindow.xaml.cs
+++ DCLibraryProject/GUI/MainWindow.xaml.cs
@@ -45,7 +45,7 @@ namespace GUI
             {
                 Console.WriteLine("Exception!");
             }
-            
+
             //Also, tell me how many entries are in the DB.
             total_text_block.Text = "Total Items: " + channel.GetNumEntries().ToString();
         }
@@ -57,7 +57,7 @@ namespace GUI
             int bal = 0;
             uint acct = 0, pin = 0;
             //On click, Get the index....
-            index = Int32.Parse(index_text_box.Text); //TODO: unhandled System.FormatException here
+            index = Int32.Parse(index_text_box.Text); //TODO: unhandled System.FormatException here check for empty and non-numerical
             //Then, run our RPC function, using the out mode parameters...
             channel.GetValuesForEntry(index, out acct, out pin, out bal, out fName, out lName);
             //And now, set the values in the GUI!
