2d95bb4e414841e52e1f5e34f370071a82b42a1a
Victor Marteli <caio.marteli@proton.me>
2023-08-28 18:40:20 +0800

    added separate thread calls, improved bitmap conversion

--- DCLibraryProject/GUI/MainWindow.xaml.cs
+++ DCLibraryProject/GUI/MainWindow.xaml.cs
@@ -9,10 +9,13 @@ using System.Windows.Interop;
 using System.Windows.Media.Imaging;
 using DCBusinessTier;
 using DCServer;
+using DCDatabase;
+using System.Security.Cryptography;
+using System.Reflection;
+using System.Threading;
 
 namespace GUI {
-    public delegate void SearchSurname(string value, out uint acctNo, out uint pin, out int bal,
-        out string fName, out string lName, out byte[] imgBytes); // Delegate for searching surname
+    public delegate DataStruct SearchSurname(string searchString); // Delegate for searching surname
     public partial class MainWindow : Window {
         private BusinessServerInterface channel;
         private ChannelFactory<BusinessServerInterface> dataFactory;
@@ -36,36 +39,53 @@ namespace GUI {
             total_text_block.Text = $"Total Entries: {channel.GetNumEntries()}";
         }
 
-        /* Convert Byte Array to Bitmap */
-        public static Bitmap ByteArrayToBitmap(byte[] byteArray) {
-            using (MemoryStream stream = new MemoryStream(byteArray)) {
-                return new Bitmap(stream);
+        /* Converts a Byte Array to a BitmapSource file that can be displayed */
+        public BitmapSource BytesToBitmapSource(byte[] byteArray) {
+            BitmapImage bitmapImage = new BitmapImage();
+            using (MemoryStream ms = new MemoryStream(byteArray)) {
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = ms;
+                bitmapImage.EndInit();
             }
+            //bitmapImage.Freeze(); //makes it shareable accross threads
+            return bitmapImage;
         }
 
-        /* Helper method to update UI fields */
-        private void UpdateFields(int index, string fName, string lName, int bal, uint acctNo, uint pin, byte[] imageBytes) {
-            index_text_box.Text = index.ToString();
-            fName_text_box.Text = fName;
-            lName_text_box.Text = lName;
-            balance_text_box.Text = bal.ToString("C");
-            acctNo_text_box.Text = acctNo.ToString();
-            pin_text_box.Text = pin.ToString("D4");
+        /* Method to update UI fields */
+        private void UpdateGUI(DataStruct user) {
+            // Update all text fields at once
+            Dispatcher.Invoke(() => {
+                acctNo_text_box.Text = user.acctNo.ToString();
+                pin_text_box.Text = user.pin.ToString("D4");
+                balance_text_box.Text = user.balance.ToString("C");
+                fName_text_box.Text = user.firstName;
+                lName_text_box.Text = user.lastName;
 
-            if (imageBytes != null) {
-                using (Bitmap image = ByteArrayToBitmap(imageBytes)) {
-                    var handle = image.GetHbitmap();
-                    image_box.Source = Imaging.CreateBitmapSourceFromHBitmap(
-                        handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                /* Updates image. ***NOTE: Code repetition but if we try to call BytesToBitmapSource() from here
+                 * we get a System.InvalidOperationException */
+                using (MemoryStream ms = new MemoryStream(user.imageBytes)) {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.BeginInit();
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.StreamSource = ms;
+                    bitmapImage.EndInit();
+                    image_box.Source = bitmapImage;
                 }
-            }
+            });
         }
 
-        private void go_index_btn_Click(object sender, RoutedEventArgs e) {
+        private void Go_btnClick(object sender, RoutedEventArgs e) {
             try {
-                int index = Int32.Parse(index_text_box.Text);
-                channel.GetValuesForEntry(index, out uint acctNo, out uint pin, out int bal, out string fName, out string lName, out byte[] imageBytes);
-                UpdateFields(index, fName, lName, bal, acctNo, pin, imageBytes);
+                channel.GetValuesForEntry(Int32.Parse(index_text_box.Text), out uint acctNo, out uint pin, out int bal,
+                    out string fName, out string lName, out byte[] imageBytes);
+                acctNo_text_box.Text = acctNo.ToString();
+                pin_text_box.Text = pin.ToString("D4");
+                balance_text_box.Text = bal.ToString("C");
+                fName_text_box.Text = fName;
+                lName_text_box.Text = lName;
+                image_box.Source = BytesToBitmapSource(imageBytes); //TODO: Might need to check for null
+
             } catch (FormatException) {
                 fName_text_box.Text = "Not a valid number";
             } catch (FaultException<IndexFault> fex) {
@@ -75,34 +95,42 @@ namespace GUI {
                 Console.WriteLine($"{cex.Message} {cex.InnerException} {cex.StackTrace}");
             }
         }
-
-        private void search_surname_btn_Click(object sender, RoutedEventArgs e) {
-            /* Initialize the delegate and callback */
-            searchSurname = channel.GetValuesForSearch;
-            AsyncCallback callback = this.OnSearchSurnameCompletion;
+        /* Search button click method, Runs on a separate thread */
+        private void Search_btnClick(object sender, RoutedEventArgs e) {
+            
+            searchSurname = SearchDB; //points delegate to SearchDB
+            AsyncCallback callback = this.OnSearchCompletion; // Initialize the delegate and callback
 
             /* Start the asynchronous operation */
-            IAsyncResult result = searchSurname.BeginInvoke(search_text_box.Text, out uint acctNo,
-            out uint pin, out int bal, out string fName, out string lName, out byte[] imgBytes, callback, null);
+            IAsyncResult result = searchSurname.BeginInvoke(search_text_box.Text, callback, null); //TODO; **searchSurname** was null.
         }
 
-        private void OnSearchSurnameCompletion(IAsyncResult asyncResult) {
+        private DataStruct SearchDB(string searchString) {
             /* Variables for storing the result */
-            uint acctNo;
-            uint pin;
-            int bal;
-            string fName;
-            string lName;
-            byte[] imgBytes;
-            int index = 0;  // TODO: Retrieve the actual index if required
+            channel.GetValuesForSearch(searchString, out uint acctNo, out uint pin, out int bal, 
+                out string fName, out string lName, out byte[] imgBytes); //delegate assigned
+            if (acctNo != 0) {
+                DataStruct user = new DataStruct();
+                user.acctNo = acctNo;
+                user.pin = pin;
+                user.balance = bal;
+                user.firstName = fName;
+                user.lastName = lName;
+                user.imageBytes = imgBytes;
+                return user;
+            }
+            return null;
+        }
 
-            /* End the asynchronous call and get the results */
-            AsyncResult asyncObj = (AsyncResult)asyncResult;
+        private void OnSearchCompletion(IAsyncResult asyncResult) {
+
+            DataStruct user = null;
+            SearchSurname searchSurname = null;            
+            AsyncResult asyncObj = (AsyncResult)asyncResult; // End the asynchronous call and get the results
             if (!asyncObj.EndInvokeCalled) {
-                searchSurname.EndInvoke(out acctNo, out pin, out bal, out fName, out lName, out imgBytes, asyncObj);
-
-                /* Update the GUI */
-                this.Dispatcher.Invoke(new Action(() => UpdateFields(index, fName, lName, bal, acctNo, pin, imgBytes)));
+                searchSurname = (SearchSurname)asyncObj.AsyncDelegate;
+                user = searchSurname.EndInvoke(asyncObj);                
+                UpdateGUI(user); // Update the GUI
             }
 
             asyncObj.AsyncWaitHandle.Close(); //Clean up
